a72615dfbd7fc7b52cc9c6bb35cc5c6c514dec5b
chetan <chetan249581@gmail.com>
2024-01-03 17:02:36 +0530

    Created Controller in MVC

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ mongoose.connect("mongodb://127.0.0.1:27017/company")
         console.log("Connected Successfully");
     })
     .catch((err)=>console.log(errorrr));
+
+
+const express=require("express");
+const app=express();
+
+const bodyparser=require("body-parser");
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+const employee=require("./controllers/employeeController");
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.get('/register',function(req,res){
+    res.render('Register');
+});
+
+app.post('/register',employee.insertEmployee)
+
+app.listen(3000,function(){
+    console.log("Server is running......");
+});
