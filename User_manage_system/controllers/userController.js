c2d3647e6ee614c48dafcf1706745289cacf0bd2
chetan <chetan249581@gmail.com>
2024-01-05 19:29:54 +0530

    added forget password feature

--- User_manage_system/controllers/userController.js
+++ User_manage_system/controllers/userController.js
@@ -1,7 +1,9 @@
 const User=require("../models/userModel");
 const bcrypt=require("bcrypt");
 const nodemailer=require("nodemailer");
-
+const randomS=require("randomstring");
+const dotenv = require("dotenv")
+dotenv.config();
 
 const securePassword=async(password)=>{
     try{
@@ -21,16 +23,50 @@ const sendVerifyMail=async(name,email,userid)=>{
             secure:false,
             requireTLS:true,
             auth:{
-                user:'silentknight7767@gmail.com',
-                pass:'mqbqlayhxkydgzyd'
+                user:process.env.Email,
+                pass: process.env.EmailPass
             }
         });
 
         const mailoptions={
-            from: 'silenknight7767@gmail.com',
+            from: process.env.Email,
             to:email,
             subject:"For Verfication Mail",
             html:'<p> Hii '+name+' , please click here to <a href="http://127.0.0.1:3000/verify?id='+userid+'">Verfiy</a>Your mail</p>'
+        }
+        transporter.sendMail(mailoptions,function(error,info){
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log("Email has been Sent: - ",info.response);
+            }
+        })
+    }
+    catch(error){
+        console.log(error.message);
+    }
+};
+
+// for reset passwrod send mail 
+const sendResetMail=async(name,email,token)=>{
+    try{
+        const transporter=nodemailer.createTransport({
+            host:'smtp.gmail.com',
+            port:587,
+            secure:false,
+            requireTLS:true,
+            auth:{
+                user:process.env.Email,
+                pass: process.env.EmailPass
+            }
+        });
+
+        const mailoptions={
+            from: process.env.Email,
+            to:email,
+            subject:"For Reset Password Mail",
+            html:'<p> Hii '+name+' , please click here to <a href="http://127.0.0.1:3000/forgetPass?token='+token+'">Reset Your Password</a>Your mail</p>'
         }
         transporter.sendMail(mailoptions,function(error,info){
             if(error){
@@ -139,7 +175,6 @@ const verifyLogin=async(req,res)=>{
 const loadHome=async(req,res)=>{
     try{
         res.render('home');
-        res.redirect('/');
     }
     catch(error){
         console.log(error.message);
@@ -156,6 +191,74 @@ const userLogout=async(req,res)=>{
     }
 }
 
+const forgetLoad=async(req,res)=>{
+    try{
+        res.render('forget');
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+const forgetVerify=async(req,res)=>{
+    try{
+        mail=req.body.email;
+        const Data=await User.findOne({email:mail});
+
+        if(Data){
+            if(Data.is_varified === 0){
+                res.render('forget',{message:"Please verify Your Email"});
+            }
+            else{
+                const randomString=randomS.generate();
+                const updated=await User.updateOne({email:mail},{$set:{token:randomString}});
+                sendResetMail(Data.name,Data.email,randomString);
+                res.render('forget',{message:"Please check your mail To reset your Password"});
+            }
+        }
+        else{
+            res.render('forget',{message:"User Mail is Incorrect"});
+        }
+
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+
+const forgetPassLoad=async(req,res)=>{
+    try{
+        const token=req.query.token;
+        const tokendata=await User.findOne({token:token});
+
+        if(tokendata){
+            res.render('forgetPass',{user_id:tokendata._id});
+        }
+        else{
+            res.render('404',{message:"Token is Invalid"});
+        }
+
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+const resetPassword=async(req,res)=>{
+    try{
+        let password=req.body.password;
+        let user_id=req.body.user_id;
+        const secure_password=await securePassword(password);
+        const user_data=await User.findByIdAndUpdate({_id:user_id},{$set:{password:secure_password,token:''}});
+
+        res.redirect('/');
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
 
 module.exports={
     loadRegister,
@@ -164,5 +267,9 @@ module.exports={
     loginLoad,
     verifyLogin,
     loadHome,
-    userLogout
+    userLogout,
+    forgetLoad,
+    forgetVerify,
+    forgetPassLoad,
+    resetPassword
 }
